6efc3babf602dff18db73bd90a4ca313aca4bb79
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-04-12 20:31:45 -0500

    updated searchCity

--- src/index.js
+++ src/index.js
@@ -55,4 +55,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Mexico City");
+searchCity("Paris");
